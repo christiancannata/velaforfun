6f1590d315d779fab2bbd439e655fa5e10899eeb
Christian Cannata <christian1488@hotmail.it>
2015-04-15 13:11:44 +0000

    wip entita

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,40 +5,41 @@ namespace AppBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="users")
  */
 class User extends BaseUser
 {
-	/**
-	 * @ORM\Id
-	 * @ORM\Column(type="integer")
-	 * @ORM\GeneratedValue(strategy="AUTO")
-	 */
-	protected $id;
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(name="facebook_id", type="string", nullable=true)
-	 */
-	private $facebookID;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="facebook_id", type="string", nullable=true)
+     */
+    private $facebookID;
 
 
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(name="nome", type="string", nullable=true)
-	 */
-	private $nome;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome", type="string", nullable=true)
+     */
+    private $nome;
 
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(name="cognome", type="string", nullable=true)
-	 */
-	private $cognome;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cognome", type="string", nullable=true)
+     */
+    private $cognome;
 
 
     /**
@@ -63,22 +64,22 @@ class User extends BaseUser
     private $privacy;
 
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->squadre = new ArrayCollection();
-		$this->competizioni = new ArrayCollection();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->squadre = new ArrayCollection();
+        $this->competizioni = new ArrayCollection();
+    }
 
-	/**
-	 * Get id
-	 *
-	 * @return integer
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
 
     /**
@@ -97,70 +98,77 @@ class User extends BaseUser
     /**
      * Get githubID
      *
-     * @return string 
+     * @return string
      */
     public function getFacebookID()
     {
         return $this->facebookID;
     }
 
-	/**
-	 * @return string
-	 */
-	public function getCognome() {
-		return $this->cognome;
-	}
+    /**
+     * @return string
+     */
+    public function getCognome()
+    {
+        return $this->cognome;
+    }
 
-	/**
-	 * @param string $cognome
-	 */
-	public function setCognome( $cognome ) {
-		$this->cognome = $cognome;
-	}
+    /**
+     * @param string $cognome
+     */
+    public function setCognome($cognome)
+    {
+        $this->cognome = $cognome;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getNome() {
-		return $this->nome;
-	}
+    /**
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
 
-	/**
-	 * @param string $nome
-	 */
-	public function setNome( $nome ) {
-		$this->nome = $nome;
-	}
+    /**
+     * @param string $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getCompetizioni() {
-		return $this->competizioni;
-	}
+    /**
+     * @return mixed
+     */
+    public function getCompetizioni()
+    {
+        return $this->competizioni;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getSquadre() {
-		return $this->squadre;
-	}
+    /**
+     * @return mixed
+     */
+    public function getSquadre()
+    {
+        return $this->squadre;
+    }
 
 
+    /**
+     * @return string
+     */
+    public function getProfilo()
+    {
+        return $this->profilo;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getProfilo() {
-		return $this->profilo;
-	}
-
-	/**
-	 * @param string $profilo
-	 */
-	public function setProfilo( $profilo ) {
-		$this->profilo = $profilo;
-	}
+    /**
+     * @param string $profilo
+     */
+    public function setProfilo($profilo)
+    {
+        $this->profilo = $profilo;
+    }
 
     /**
      * @return string
