2a0b288a91c17974c926687b242d028d91d64ec5
christiancannata <christian.cannata@facile.it>
2015-04-15 12:57:46 +0200

    fixed entita

--- src/AppBundle/Entity/Porto.php
+++ src/AppBundle/Entity/Porto.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="Porto")
+ * @ORM\Table(name="porto")
  */
 class Parola
 {
--- src/AppBundle/Entity/Segnale.php
+++ src/AppBundle/Entity/Segnale.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="Segnale")
+ * @ORM\Table(name="segnale")
  */
 class Segnale {
 	/**
