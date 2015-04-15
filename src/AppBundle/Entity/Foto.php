f7fcd5a8cb528bbc8ab45ec37dc9d734b5e36640
christiancannata <christian.cannata@facile.it>
2015-04-15 15:49:46 +0200

    aggiunta entita foto

---
+++ src/AppBundle/Entity/Foto.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Acme\DemoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="foto")
+ */
+class Foto {
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="path", type="string", nullable=true)
+	 */
+	private $path;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome", type="string", nullable=true)
+     */
+    private $nome;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="in_evidenza", type="integer", nullable=true)
+	 */
+	private $inEvidenza;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="timestamp", type="datetime", nullable=true)
+     */
+    private $timestamp;
+
+
+	public function __construct() {
+		parent::__construct();
+		// your own logic
+	}
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * @param string $link
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInEvidenza()
+    {
+        return $this->inEvidenza;
+    }
+
+    /**
+     * @param string $inEvidenza
+     */
+    public function setInEvidenza($inEvidenza)
+    {
+        $this->inEvidenza = $inEvidenza;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @param string $timestamp
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param string $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+
+
+
+    
+
+
+
+
+}
