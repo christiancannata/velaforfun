c460e962f5f5317ae825cc85ea67ebe2754614ef
christiancannata <christian.cannata@facile.it>
2015-04-21 19:00:52 +0200

    mappa porti funziona bravo

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,10 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ivory\GoogleMap\Map;
+use Ivory\GoogleMap\Overlays\Animation;
+use Ivory\GoogleMap\Overlays\Marker;
+use Ivory\GoogleMap\Helper\MapHelper;
 
 class DefaultController extends Controller
 {
@@ -18,6 +22,36 @@ class DefaultController extends Controller
 
         $articoli=$repository->findAll();
 
-        return $this->render('default/index.html.twig',array("articoli"=>$articoli));
+
+
+
+        $map = new Map();
+        $map->setAsync(true);
+        $map->setLanguage("ita");
+        $porti = $this->getDoctrine()
+            ->getRepository('AppBundle:Porto')->findAll();
+
+        foreach($porti as $porto){
+            $marker = new Marker();
+
+// Configure your marker options
+            $marker->setPrefixJavascriptVariable('marker_');
+            $marker->setPosition($porto->getLatitudine(), $porto->getLongitudine(), true);
+            $marker->setAnimation(Animation::DROP);
+            $marker->setOption('clickable', false);
+            $marker->setOption('flat', true);
+            $marker->setOptions(array(
+                'clickable' => false,
+                'flat'      => true,
+            ));
+            $map->addMarker($marker);
+
+        }
+
+
+
+        $mapHelper = new MapHelper();
+
+        return $this->render('default/index.html.twig',array("articoli"=>$articoli,"mapHelper"=>$mapHelper,"map"=>$map));
     }
 }
--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ivory\GoogleMap\Map;
+use Ivory\GoogleMap\Overlays\Animation;
+use Ivory\GoogleMap\Overlays\Marker;
 
 class DefaultController extends Controller
 {
@@ -10,6 +13,32 @@ class DefaultController extends Controller
     {
         $repository = $this->getDoctrine()
             ->getRepository('BlogBundle:Articolo');
-        return $this->render('BlogBundle:Default:index.html.twig', array('name' => $name));
+
+        $map = new Map();
+
+        $porti = $this->getDoctrine()
+            ->getRepository('AppBundle:Porto')->findAll();
+
+        foreach($porti as $porto){
+            $marker = new Marker();
+
+// Configure your marker options
+            $marker->setPrefixJavascriptVariable('marker_');
+            $marker->setPosition($porto->getLatitudine(), $porto->getLongitudine(), true);
+            $marker->setAnimation(Animation::DROP);
+
+            $marker->setOption('clickable', false);
+            $marker->setOption('flat', true);
+            $marker->setOptions(array(
+                'clickable' => false,
+                'flat'      => true,
+            ));
+            $map->addMarker($marker);
+
+        }
+
+
+
+        return $this->render('BlogBundle:Default:index.html.twig', array('name' => $name,''));
     }
 }
