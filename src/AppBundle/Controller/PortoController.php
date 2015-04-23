626eacf54768f1632e565d56d003535709550ea0
christiancannata <christian.cannata@facile.it>
2015-04-23 10:24:03 +0200

    parametri configurati

---
+++ src/AppBundle/Controller/PortoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Form\PortoType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Controller\BaseController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class PortoController extends BaseController
+{
+    /**
+     * @Route( "crea", name="create_post" )
+     * @Template()
+     */
+    public function createAction(Request $request)
+    {
+        return $this->postForm($request,new PortoType() );
+    }
+}
