36b4ea7c3f9a8a7e1bf142d263734feaeabecd6d
christiancannata <christian.cannata@facile.it>
2015-05-18 17:40:47 +0200

    wip frontend

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -1,7 +1,37 @@
 jQuery(document).ready(function ($) {
 
+    WebFontConfig = {
+        google: {families: ['Chivo:400,400italic,900:latin']}
+    };
+    (function () {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    })();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".flip-container").addClass("hover");
+            $(".front,.flip-container").css("height", "50px");
+            $("#nav-top").addClass("shadow-menu");
+            $('#sub-menu').fadeIn(500);
+        } else {
+
+
+            $(".flip-container").removeClass("hover");
+            $(".front,.flip-container").css("height", "50px");
+            $('#sub-menu').fadeOut(500);
+            $("#nav-top").removeClass("shadow-menu");
+        }
+    });
+
+
     $(".delete-entity").click(function () {
-        var button=$(this);
+        var button = $(this);
         $("button").attr("disabled", "disabled");
         $.ajax({
             type: "POST",
@@ -20,4 +50,39 @@ jQuery(document).ready(function ($) {
         });
     });
 
+
+    $(".dropdown-toggle").click(function () {
+        if ($(this).find("i").hasClass("fa-bars")) {
+            $(this).find("i").switchClass("fa-bars", "fa-arrow-left");
+        } else {
+            $(this).find("i").switchClass("fa-arrow-left", "fa-bars");
+        }
+    });
+
+
+    var mapmargin = 50;
+    $('#map').css("height", ($(window).height() - mapmargin));
+    $(window).on("resize", resize);
+    resize();
+    function resize() {
+
+        if ($(window).width() >= 980) {
+            $('#map').css("height", ($(window).height() - mapmargin));
+            $('#map').css("margin-top", 50);
+        } else {
+            $('#map').css("height", ($(window).height() - (mapmargin + 12)));
+            $('#map').css("margin-top", -21);
+        }
+
+    }
+
+
+
+    $.get('/json/portiItaliani.json', function (data) {
+        $("#selectPorto").typeahead({source: data});
+    }, 'json');
+
+
+
+    $('.scroll-pane').jScrollPane();
 });
