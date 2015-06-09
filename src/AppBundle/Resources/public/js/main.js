027cb64550b4ffba5048351e4b169746d495b276
christiancannata <christian.cannata@facile.it>
2015-06-09 18:27:22 +0200

    wip grafica

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -1,8 +1,8 @@
+
 jQuery(document).ready(function ($) {
 
 
     $(".marker-description button").click(function(){
-        alert("jjj");
         location.href=$(this).attr("id");
     });
 
@@ -11,32 +11,7 @@ jQuery(document).ready(function ($) {
         window.history.back();
     });
 
-    (function($) {
 
-      if( $('.arrow_box').length >0){
-
-          var element = $('.arrow_box'),
-              originalY = element.offset().top;
-          // Space between element and top of screen (when scrolling)
-          var topMargin = 200;
-
-          // Should probably be set in CSS; but here just for emphasis
-          element.css('position', 'relative');
-
-          $(window).on('scroll', function(event) {
-              var scrollTop = $(window).scrollTop();
-
-                    element.stop(false, false).animate({
-                        top: scrollTop < originalY
-                            ? 0
-                            : scrollTop - originalY + topMargin
-                    }, 300);
-
-          });
-
-      }
-
-    })(jQuery);
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
