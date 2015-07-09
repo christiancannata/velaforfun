67fb82adaf4aaad52d2d883a97052fd710f6ed8f
christiancannata <christian.cannata@facile.it>
2015-07-09 15:27:22 +0200

    minor fix

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -19,6 +19,15 @@ meteoIcon["11n"] = "wi-thunderstorm";
 meteoIcon["13n"] = "wi-snow";
 meteoIcon["50n"] = "wi-fog";
 
+function goToByScroll(id){
+    // Remove "link" from the ID
+    id = id.replace("link", "");
+    // Scroll
+    $('html,body').animate({
+            scrollTop: $("#"+id).offset().top},
+        'slow');
+}
+
 
 function vota(voto){
     var button = $(this);
