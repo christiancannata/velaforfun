ae83ad823c989de200fb4e3c39dd2f8639c98ecc
christiancannata <christian.cannata@facile.it>
2015-07-22 16:01:35 +0200

    minor fix

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Controller\BaseController;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends BaseController
 {
@@ -19,8 +20,34 @@ class DefaultController extends BaseController
             ->getRepository('BlogBundle:Categoria')->findAll();
 
         $ultimiArticoli = $this->getDoctrine()
-            ->getRepository('BlogBundle:Articolo')->findAll(array("id"=>"desc"), 5);
+            ->getRepository('BlogBundle:Articolo')->findAll(array("id"=>"desc"), 10);
 
-        return $this->render('BlogBundle:Default:index.html.twig', array('categorie' => $categorie,'ultimiArticoli'=>$ultimiArticoli));
+        return $this->render('BlogBundle:Default:index.html.twig', array('categorie' => $categorie,'articoli'=>$ultimiArticoli));
+    }
+
+
+    /**
+     * @Route("/json", name="archivio_json")
+     */
+    public function getArticoliJsonAction( Request $r)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+
+        $articoli = $em->getRepository('BlogBundle:Articolo')->findBy(
+            array('stato' => "ATTIVO"),
+            array('lastUpdateTimestamp' => 'desc'),
+            3,
+            $r->get('offset')
+        );
+
+
+        return $this->render(
+            'BlogBundle:Categoria:articoli-ajax.html.twig',
+            array(
+                'articoli' => $articoli,
+            )
+        );
+
     }
 }
