b66438bcf35614b8a0e9bc0453bd14fb013b97b0
christiancannata <christian.cannata@facile.it>
2015-07-23 15:26:49 +0200

    minor fix

--- src/AppBundle/EventListener/VelaForumListener.php
+++ src/AppBundle/EventListener/VelaForumListener.php
@@ -10,6 +10,12 @@ use CCDNForum\ForumBundle\Component\Dispatcher\Event\UserTopicEvent;
 class VelaForumListener implements EventSubscriberInterface
 {
 
+    protected $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
 
 
     /**
@@ -19,7 +25,8 @@ class VelaForumListener implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            ForumEvents::USER_TOPIC_CREATE_COMPLETE            => 'onTopicCreateComplete',
+            ForumEvents::USER_TOPIC_CREATE_COMPLETE => 'onTopicCreateComplete',
+            ForumEvents::USER_TOPIC_REPLY_COMPLETE => 'onTopicReplyComplete',
         );
     }
 
@@ -35,4 +42,42 @@ class VelaForumListener implements EventSubscriberInterface
 
         }
     }
+
+    /**
+     *
+     * @access public
+     * @param \CCDNForum\ForumBundle\Component\Dispatcher\Event\UserTopicEvent $event
+     */
+    public function onTopicReplyComplete(UserTopicEvent $event)
+    {
+        if ($event->getTopic()) {
+
+            $post = $this->container->get('doctrine')
+                ->getRepository('CCDNForumForumBundle:Post')->findByTopic($event->getTopic());
+
+            $ultimoPost = array_pop($post);
+            foreach ($post as $singlePost) {
+
+                $mailer = $this->container->get('mailer');
+                $messaggio = $mailer->createMessage()
+                    ->setSubject("Risposta al topic: ".$event->getTopic()->getTitle())
+                    ->setFrom('info@velaforfun.com')
+                    ->setTo($singlePost->getCreatedBy()->getEmail())
+                    ->setBcc('christian1488@hotmail.it')
+                    ->setBody(
+                        $this->container->get('templating')->render(
+                        // app/Resources/views/Emails/registrazione.html.twig
+                            'Emails/risposta_post.html.twig',
+                            array('topic' => $event->getTopic(), 'post' => $ultimoPost)
+                        ),
+                        'text/html'
+                    );
+                $mailer->send($messaggio);
+
+            }
+
+
+        }
+    }
+
 }
