622eec136fb3c3e71bf1cf93c2df61f9f1ef9050
christiancannata <christian.cannata@facile.it>
2015-10-05 19:11:48 +0200

    minor fix

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,7 +34,7 @@ class AppKernel extends Kernel
             new Sonata\IntlBundle\SonataIntlBundle(),
             new NewsletterBundle\NewsletterBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
-            new WallPosterBundle\WallPosterBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -42,6 +42,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new WallPosterBundle\WallPosterBundle(),
         }
 
         return $bundles;
