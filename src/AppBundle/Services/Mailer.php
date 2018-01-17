5a166481ce9e5f1efaf8050085748fc79398bab6
christian cannata <christiancannata@MBPdichristian.station>
2018-01-17 16:50:26 +0100

    minor fix

---
+++ src/AppBundle/Services/Mailer.php
@@ -0,0 +1,12 @@
+<?php
+namespace AppBundle\Services;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
+
+class WeatherService extends ContainerAware
+{
+
+
+
+}
